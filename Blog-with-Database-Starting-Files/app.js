69ebc55ad831e41dae3260530b78fb508cd39ad7
Tanner Anderson <tra.assets@gmail.com>
2020-09-26 19:40:25 -0600

    Add gitignore, procfile and update ports

--- Blog-with-Database-Starting-Files/app.js
+++ Blog-with-Database-Starting-Files/app.js
@@ -7,7 +7,9 @@ const _ = require("lodash");
 const mongo = require("mongodb")
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/blogDB", {useNewUrlParser: true});
+mongoose.connect("mongodb+srv://admin-tanner:Test123@cluster0.e9x2d.mongodb.net/blogDB", {
+  useNewUrlParser: true
+});
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -17,7 +19,9 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(express.static("public"));
 
 
@@ -28,9 +32,9 @@ const postsSchema = {
 
 const Post = mongoose.model("Post", postsSchema);
 
-app.get("/", function(req, res){
+app.get("/", function(req, res) {
 
-  Post.find({}, function(err, results){
+  Post.find({}, function(err, results) {
     if (err) {
       console.log(err);
     } else {
@@ -39,35 +43,39 @@ app.get("/", function(req, res){
     res.render("home", {
       startingContent: homeStartingContent,
       posts: results
-      });
+    });
   })
 
 });
 
-app.get("/about", function(req, res){
-  res.render("about", {aboutContent: aboutContent});
+app.get("/about", function(req, res) {
+  res.render("about", {
+    aboutContent: aboutContent
+  });
 });
 
-app.get("/contact", function(req, res){
-  res.render("contact", {contactContent: contactContent});
+app.get("/contact", function(req, res) {
+  res.render("contact", {
+    contactContent: contactContent
+  });
 });
 
-app.get("/compose", function(req, res){
+app.get("/compose", function(req, res) {
   res.render("compose");
 });
 
-app.post("/compose", function(req, res){
+app.post("/compose", function(req, res) {
   const post = {
     title: req.body.postTitle,
     content: req.body.postBody
   };
 
-const newPost = new Post ({
-  postTitle: post.title,
-  postContent: post.content
-});
-  newPost.save(function(err){
-    if (!err){
+  const newPost = new Post({
+    postTitle: post.title,
+    postContent: post.content
+  });
+  newPost.save(function(err) {
+    if (!err) {
       res.redirect("/");
     }
   });
@@ -76,10 +84,12 @@ const newPost = new Post ({
 
 });
 
-app.get("/posts/:postId", function(req, res){
+app.get("/posts/:postId", function(req, res) {
   const requestedPostId = req.params.postId
 
-  Post.findOne({_id: requestedPostId}, function(err, post){
+  Post.findOne({
+    _id: requestedPostId
+  }, function(err, post) {
     res.render("post", {
       title: post.postTitle,
       content: post.postContent
@@ -88,6 +98,10 @@ app.get("/posts/:postId", function(req, res){
 
 });
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port, function(){
+  console.log("Server started successfully");
 });
